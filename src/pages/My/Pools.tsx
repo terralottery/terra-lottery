5ebf3070b722ed4434f2e4cd43c57d377b79ce1d
Tim Juretzek <tj@jur-solutions.de>
2021-05-06 13:50:47 +0200

    added my Account overview

--- src/pages/My/Pools.tsx
+++ src/pages/My/Pools.tsx
@@ -1,14 +1,22 @@
 import { UUSD } from "../../constants"
 import MESSAGE from "../../lang/MESSAGE.json"
 import { gt } from "../../libs/math"
-import { formatAsset } from "../../libs/parse"
+import { format, formatAsset } from "../../libs/parse"
 import { getPath, MenuKey } from "../../routes"
 
 import Card from "../../components/Card"
 import Dl from "../../components/Dl"
 import LinkButton from "../../components/LinkButton"
-import { menu as poolsMenu, MenuKey as PoolsMenuKey } from "../Pools"
+import { menu as poolsMenu, MenuKey as PoolsMenuKey, poolName } from "../Pools"
 import NoAssets from "./NoAssets"
+import { useBank } from "../../contexts/bank"
+import Table from "../../components/Table"
+import { percent } from "../../libs/num"
+import { useMemo } from "react"
+import big from "big.js"
+import { currentAPY } from "../Dashboard/DashboardHeader"
+import { useConstants } from "../../contexts/contants"
+import { useInterest } from "../../graphql/queries/interest"
 
 interface Props {
   totalTickets: string
@@ -21,7 +29,8 @@ interface Props {
 const Pools = ({ loading, dataSource, ...props }: Props) => {
   const { totalRewards, totalWinnings, totalTickets } = props
   const totalEarnings = Number(totalRewards) + Number(totalWinnings)
-
+  const bank = useBank()
+  const { blocksPerYear } = useConstants()
   const claimAll = {
     to: getPath(MenuKey.POOLS) + poolsMenu[PoolsMenuKey.CLAIMALL].path,
     className: "desktop",
@@ -31,7 +40,15 @@ const Pools = ({ loading, dataSource, ...props }: Props) => {
     size: "sm" as const,
     outline: true,
   }
+  const {
+    data: { marketStatus },
+  } = useInterest()
+  const apy = useMemo(() => currentAPY(marketStatus, blocksPerYear), [
+    blocksPerYear,
+    marketStatus,
+  ])
 
+  const ticketApy = Number(big(apy).toFixed()) / 2
   const dataExists = !!dataSource.length
   const description = dataExists && (
     <Dl
@@ -52,6 +69,16 @@ const Pools = ({ loading, dataSource, ...props }: Props) => {
     />
   )
 
+  const lotteryPositions = [
+    {
+      lottery: poolName.T7UST,
+      apy: ticketApy,
+      balance: bank.userBalances.uaUST.toString(),
+      value: totalTickets,
+      chances: "1:17,837",
+    },
+  ]
+
   return (
     <Card
       title="Your Lottery Earnings"
@@ -59,10 +86,58 @@ const Pools = ({ loading, dataSource, ...props }: Props) => {
       description={description}
       loading={loading}
     >
-      <NoAssets
-        description={MESSAGE.MyPage.Empty.Staked}
-        link={MenuKey.POOLS}
-      />
+      {Number(bank.userBalances.uaUST.toString()) > 0 ? (
+        <Table
+          columns={[
+            {
+              key: "lottery",
+              title: "Lottery",
+              render: (value) => {
+                return value
+              },
+              bold: true,
+            },
+            {
+              key: "apy",
+              title: "APY",
+              render: (value) => {
+                return percent(value)
+              },
+              align: "center",
+            },
+            {
+              key: "balance",
+              title: "Tickets",
+              render: (value) => {
+                return format(value, "Tickets")
+              },
+              align: "right",
+            },
+            {
+              key: "value",
+              title: "Current Value",
+              render: (value) => {
+                return formatAsset(value, UUSD)
+              },
+              align: "right",
+            },
+            {
+              key: "chances",
+              title: "Winning Odds",
+              render: (value) => {
+                return value
+              },
+              align: "right",
+            },
+          ]}
+          dataSource={lotteryPositions}
+        />
+      ) : (
+        <NoAssets
+          description={MESSAGE.MyPage.Empty.Staked}
+          link={MenuKey.POOLS}
+        />
+      )}
     </Card>
   )
 }
